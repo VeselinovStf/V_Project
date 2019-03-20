a7806100c13d4a03b556a3061198b23dfbdf2ed4
VeselinovStf <veselinovStf@gmail.com>
2019-03-20 11:51:13 +0200

    Added Presentation layer with identity and Data layer

---
+++ EstateSystem_Alpha_V01_01/ES.Data/Identity/EstateSystemIdentityUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ES.Data.Identity
+{
+    // Add profile data for application users by adding properties to the ESWebUser class
+    public class EstateSystemIdentityUser : IdentityUser
+    {
+    }
+}
---
+++ EstateSystem_Alpha_V01_01/ES.Web/ViewModels/Identity/ForgotPasswordViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ES.Web.ViewModels.Identity
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
